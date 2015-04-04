af65309ac6c2acc1bb6a575d888492a3e0d9beb8
Lin Qiu <lin@kitcheck.com>
2015-04-04 14:57:24 -0400

    Make box walk!

--- src/js/game/app.js
+++ src/js/game/app.js
@@ -1,12 +1,12 @@
-var Phaser = require('Phaser')
-  , _ = require('lodash')
-  , properties = require('./properties')
-  , states =
-    { boot: require('./states/boot.js')
-    , preloader: require('./states/preloader.js')
-    , game: require('./states/game.js')
-    }
-  , game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, 'game');
+var Phaser = require('Phaser'),
+    _ = require('lodash'),
+    properties = require('./properties'),
+    states ={
+      boot: require('./states/boot.js'),
+      preloader: require('./states/preloader.js'),
+      game: require('./states/game.js')
+    },
+    game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, 'game');
 
 // Automatically register each state.
 _.each(states, function(state, key) {
--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,11 +1,40 @@
 module.exports = function(game) {
 
-  var gameState = {};
+  var gameState = {},
+      player;
 
   gameState.create = function () {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    player = game.add.sprite(0, 0, 'hero');
+    game.physics.arcade.enable(player);
   };
+
+  gameState.update = function() {
+    var cursors = game.input.keyboard.createCursorKeys();
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+
+    if (cursors.left.isDown) {
+      // move west
+      player.body.velocity.x = -150;
+    }
+    else if (cursors.right.isDown) {
+      // move east
+      player.body.velocity.x = 150;
+    }
+    else if (cursors.up.isDown) {
+      // move north
+      player.body.velocity.y = -150;
+    }
+    else if (cursors.down.isDown) {
+      // move south
+      player.body.velocity.y = 150;
+    }
+    else {
+
+    }
+  }
 
   return gameState;
 };
