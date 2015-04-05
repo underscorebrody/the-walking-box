18ef68cd87699c8e8e2ff25eb07cea656438c3fb
Lin Qiu <lin@kitcheck.com>
2015-04-05 15:42:44 -0400

    Got rid of custom calculations...
    turns out setting the anchor to the middle was enough to use
    the original collision detection

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -20,88 +20,9 @@ module.exports = function(game) {
     entity.body.velocity.y = 0;
   };
 
-  function repelEntity(entity) {
-    entity.body.velocity.x = -1*entity.body.velocity.x;
-    entity.body.velocity.y = -1*entity.body.velocity.x;
-  }
-
   function killZombie(bullet, zombie) {
     bullet.kill();
     zombie.kill();
-  }
-
-  function getPowDistance(fromX, fromY, toX, toY){
-    var a = Math.abs(fromX - toX);
-    var b = Math.abs(fromY - toY);
-    return (a * a) + (b * b);
-  }
-
-  function collidesRectCircle(rect, circle){
-    var radius = circle.width * 0.5;
-    var upperRectRadius = Math.max(rect.width, rect.height) * 0.75;
-
-    // quick check, whether collision is actually possible:
-    if(Math.abs(circle.x - rect.x) < radius + upperRectRadius &&
-      Math.abs(circle.y - rect.y) < radius + upperRectRadius){
-
-      // adjust radians:
-      var rotation = rect.rotation > 0 ? -1 * rect.rotation : -1 * rect.rotation + Math.PI;
-
-      // rotate circle around origin of the rectangle:
-      var rotatedCircleX = Math.cos(rotation) * (circle.x - rect.x) -
-          Math.sin(rotation) * (circle.y - rect.y) + rect.x;
-      var rotatedCircleY  = Math.sin(rotation) * (circle.x - rect.x) +
-          Math.cos(rotation) * (circle.y - rect.y) + rect.y;
-
-      // get upper left position of the rectangle:
-      var rectX = rect.x - (rect.width * 0.5);
-      var rectY = rect.y - (rect.height * 0.5);
-
-      // find closest point in the rectangle to the rotated circle's center:
-      var closestX, closestY;
-
-      if (rotatedCircleX  < rectX){
-        closestX = rectX;
-      } else if (rotatedCircleX  > rectX + rect.width){
-        closestX = rectX + rect.width;
-      } else {
-        closestX = rotatedCircleX;
-      }
-
-      if (rotatedCircleY < rectY){
-        closestY = rectY;
-      } else if (rotatedCircleY > rectY + rect.height) {
-        closestY = rectY + rect.height;
-      } else {
-        closestY = rotatedCircleY;
-      }
-
-      // check distance between closest point and rotated circle's center:
-      var distance = getPowDistance(rotatedCircleX, rotatedCircleY, closestX, closestY);
-      if (distance < radius * radius){
-        return true; // collision detected!
-      }
-    }
-    return false;
-  }
-
-  function detectCollisionOnPlayer() {
-    _.each(staticObjects.children, function(staticObject) {
-      if(collidesRectCircle(player, staticObject)) {
-        repelEntity(player);
-      }
-    });
-    _.each(buildings.children, function(building) {
-      if(collidesRectCircle(building, player)) {
-        repelEntity(player);
-      }
-    });
-    _.each(zombies.children, function(zombie) {
-      if(collidesRectCircle(zombie, player)) {
-        repelEntity(player);
-        repelEntity(zombie);
-      }
-    });
   }
 
   gameState.create = function () {
@@ -130,7 +51,6 @@ module.exports = function(game) {
           var car = staticObjects.create(coordinates[0]-randX, coordinates[1]-randY, 'car-'+_.random(1,2));
           game.physics.enable(car, Phaser.Physics.ARCADE);
           car.body.immovable = true;
-          car.pivot.setTo(75, 75);
         } else {
           var tree = staticObjects.create(coordinates[0]-randX, coordinates[1]-randY, 'tree');
           game.physics.enable(tree, Phaser.Physics.ARCADE);
@@ -142,13 +62,12 @@ module.exports = function(game) {
         game.physics.enable(building, Phaser.Physics.ARCADE);
         building.body.immovable = true;
         building.hasSpawned = false;
-        building.pivot.setTo(125, 125);
       }
     });
 
     //Create player in center area
     player = game.add.sprite(game.world.centerX, game.world.centerY, 'hero');
-    player.pivot.setTo(12,12);
+    player.anchor.setTo(0.5,0.5);
 
     game.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
@@ -163,6 +82,10 @@ module.exports = function(game) {
     resetEntity(player);
 
     game.camera.follow(player);
+
+    game.physics.arcade.collide(player, staticObjects);
+    game.physics.arcade.collide(player, zombies);
+    game.physics.arcade.collide(player, buildings);
 
     game.physics.arcade.collide(zombies, staticObjects);
     game.physics.arcade.collide(zombies, zombies);
@@ -180,8 +103,6 @@ module.exports = function(game) {
     _.each(zombies.children, function(zombie){
       zombieLogic.moveZombie(player, zombie);
     });
-
-    detectCollisionOnPlayer();
 
     _.each(buildings.children, function(building) {
       buildingLogic.spawnZombiesFromBuilding(game, zombies, player, building);
