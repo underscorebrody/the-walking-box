9971c3fdd45ea458055188cd1330322e271bebbd
Sean Perkins <sean@mobility-labs.com>
2015-04-06 23:49:23 -0400

    Express instead of connect

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic('./build')).listen(process.env.PORT);
+var express = require('express');
+//var server = express.createServer();
+// express.createServer()  is deprecated. 
+var server = express(); // better instead
+server.configure(function(){
+  server.use('/build', express.static(__dirname + '/build'));
+});
+
+server.listen(process.env.PORT);
