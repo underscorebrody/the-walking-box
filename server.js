46e1dca176cc005127e9461f7cfa135bd528e1d5
Sean Perkins <sean@mobility-labs.com>
2015-04-07 00:12:48 -0400

    Heroku bug, thanks for that

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-var express = require('express');
-//var server = express.createServer();
-// express.createServer()  is deprecated. 
-var server = express(); // better instead
-server.configure(function(){
-  server.use('/build', express.static(__dirname + '/build'));
-});
-
-server.listen(process.env.PORT);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic('./build')).listen(process.env.PORT);
